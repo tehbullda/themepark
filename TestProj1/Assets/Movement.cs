ac8755b3720ab73be3d89a6a6365881fa13a6bf0
Bullda <bullda_94@hotmail.com>
2014-04-21 22:01:25 +0200

    rollback

--- TestProj1/Assets/Movement.cs
+++ TestProj1/Assets/Movement.cs
@@ -15,18 +15,17 @@ public class Movement : MonoBehaviour {
     float horizontalSpeed = 2.0f;
     float verticalSpeed = 2.0f;
     float speed = 10.0f;
-	float rotationSpeed = 100.0f;
 	void Update () {
 		// Get the horizontal and vertical axis
 		// By default they are mapped to the arrow keys
 		// The value is in the range -1 to 1
-		float translation = Input.GetAxis ("Vertical") * speed;
+		//float translation = Input.GetAxis ("Vertical") * speed;
 		
 		// Make it move 10 meters per second instead of 10 meters per frame
-		translation *= Time.deltaTime;
+		//translation *= Time.deltaTime;
 		
 		// Move translation along the object's z-axis
-		transform.Translate (0, 0, translation);
+		//transform.Translate (0, 0, translation);
 		// Rotate around our y-axis
 		float h = horizontalSpeed * Input.GetAxis ("Mouse X");
 		float v = verticalSpeed * Input.GetAxis ("Mouse Y");
