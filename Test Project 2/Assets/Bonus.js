d43ed1863ad02955986be4566fcd7b31598fb27e
Anciny <zergoz@hotmail.com>
2014-04-23 18:53:40 +0200

    Test Project 2

---
+++ Test Project 2/Assets/Bonus.js
@@ -0,0 +1,11 @@
+﻿#pragma strict
+
+var speed : float = 5.0f;
+
+function Update() {
+	transform.Translate(Vector3(0, 0, -1) * Time.deltaTime * speed);
+}
+
+function Hit () {
+	Destroy (gameObject);
+}
