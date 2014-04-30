534673283914e07b5e2b37bdc508253d96bfe24d
Anciny <zergoz@hotmail.com>
2014-04-30 10:30:53 +0200

    Yo

--- Test Project 2/Assets/Melee.js
+++ Test Project 2/Assets/Melee.js
@@ -3,7 +3,15 @@
 var distance : float;
 var maxDistance : float = 0.15f;
 
+var PositionX : float;
+var PositionY : float;
+var PositionZ : float;
+
 function Update() {
+	PositionX = transform.position.x;
+	PositionY = transform.position.y;
+	PositionZ = transform.position.z;
+	
 	var hit : RaycastHit;
 	
 	if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), hit)) {
