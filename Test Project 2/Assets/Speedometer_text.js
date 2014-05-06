9fa096de7a09038b6606a41fa0339582256ffdbe
Bullda <bullda_94@hotmail.com>
2014-05-06 23:48:14 +0200

    HUD - speed indicator

--- Test Project 2/Assets/Speedometer_text.js
+++ Test Project 2/Assets/Speedometer_text.js
@@ -11,8 +11,30 @@
 	}
 	
 	// Update is called once per frame
+	//float volume = m_slider.getPosition().x - m_sprite.getPosition().x;
+	//volume /= (float)m_sprite.getTextureRect().width;
+	//volume *=  1.2048192771084337349397590361446f;
+	//volume -= 0.04;
+	
 	function Update () {
 		var speed : float = Melee.player_speed;
 		var speedneeded : float = SpawnTarget.target_strength;
-		speed_indicator.transform.Translate(Vector3(speed * Time.deltaTime, 0,0));
+		Debug.Log(speedneeded);
+				
+		var percentage : float = speed / speedneeded;
+		var nextpos : float = 8f * percentage;
+		if (percentage < 1.0f) {
+			nextpos = (3.5f * percentage) - 4f;
+		}
+		else if (percentage >= 1f && percentage < 2f) {
+			percentage -= 1f;
+			nextpos = (4.5f * percentage) - 0.5f;
+		}
+		else {
+			nextpos = 4f;
+		}
+		//if (nextpos > 4f) {
+			//nextpos = 4f;
+		//}
+		speed_indicator.transform.localPosition = new Vector3(nextpos, 0,0);
 	}
