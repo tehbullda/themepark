15b9056a0fa1484614103514e3cab0ea7691390a
Bullda <bullda_94@hotmail.com>
2014-05-06 17:09:51 +0200

    HUD - speed indicator WIP

--- Test Project 2/Assets/Speedometer_text.js
+++ Test Project 2/Assets/Speedometer_text.js
@@ -1,21 +1,18 @@
 ﻿#pragma strict
 
-
-	public var obj : TextMesh;
+	private var speed_indicator : GameObject;
 	// Use this for initialization
 	function Start () {
-	
+	 	for (var i = 0; i < transform.childCount; i++) {
+	 		if (transform.GetChild(i).name == "Speed_indicator") {
+	 			speed_indicator = transform.GetChild(i).gameObject;
+	 		}
+	 	}
 	}
 	
 	// Update is called once per frame
 	function Update () {
 		var speed : float = Melee.player_speed;
 		var speedneeded : float = SpawnTarget.target_strength;
-		if (speed > speedneeded) {
-			obj.color = Color.green;
-		}
-		else { 
-			obj.color = Color.red;
-		}
-		obj.text = "Speed: " + speed.ToString();
+		speed_indicator.transform.Translate(Vector3(speed * Time.deltaTime, 0,0));
 	}
--- Test Project 2/Assets/misscounter.js
+++ Test Project 2/Assets/misscounter.js
@@ -4,12 +4,16 @@ var textureArray : Texture2D[];
 var current : int = 0;
 function Start() {
 	renderer.material.mainTexture = textureArray[current];
+	renderer.material.shader = Shader.Find("Transparent/Diffuse");
+	renderer.material.color.a = 1;
+
 }
 
 function Update () {
 	if (current != Melee.current_misses) {
 		renderer.material.mainTexture = textureArray[Melee.current_misses];
-		renderer.material.shader = Shader.Find("Transparent");
+		renderer.material.shader = Shader.Find("Transparent/Diffuse");
+		renderer.material.color.a = 1.0f;
 		current = Melee.current_misses;	
 	}
 }
