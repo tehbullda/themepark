5d3df015c74f79db7631320a9223088f5e25a4d3
Bullda <bullda_94@hotmail.com>
2014-05-19 22:06:01 +0200

    almost beta2

--- Test Project 2/Assets/Ground.js
+++
@@ -1,9 +0,0 @@
-﻿#pragma strict
-
-function Update() {
-	transform.Translate(Vector3(0, 0, -1) * Time.deltaTime * Melee.player_speed, Space.World);
-	
-	if (transform.localPosition.z <= -75) {
-		transform.localPosition.z += 240;
-	}
-}
--- Test Project 2/Assets/showhighscore.js
+++
@@ -1,9 +0,0 @@
-﻿#pragma strict
-public var obj : TextMesh;
-function Start () {
-	obj.text = "HighScore: " + PlayerPrefs.GetInt("High Score").ToString();
-}
-
-function Update () {
-	
-}
