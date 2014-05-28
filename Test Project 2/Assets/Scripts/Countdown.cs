4bc672a6e5827fa0c2dd743332c4e9ffe3f12036
Bullda <bullda_94@hotmail.com>
2014-05-28 12:59:31 +0200

    scorescreen

--- Test Project 2/Assets/Scripts/Countdown.cs
+++ Test Project 2/Assets/Scripts/Countdown.cs
@@ -4,15 +4,21 @@ using System.Collections;
 public class Countdown : MonoBehaviour {
 
 	public TextMesh obj;
-	private int countdown;
+	private int countdownToPic;
+	private float countdownToMain = 4;
 	
 	void Update () {
+		if (countdownToMain <= 0.0f) {
+			Application.LoadLevel(0);		
+		}
 		if (ZigImageViewer.waitTime >= 0.0f) {
-			countdown = (int)ZigImageViewer.waitTime + 1;
-			obj.text = countdown.ToString();
+			countdownToPic = (int)ZigImageViewer.waitTime + 1;
+			obj.text = countdownToPic.ToString();
 		}
 		else {
 			obj.text = "";
+			countdownToMain -= Time.deltaTime;
+			obj.text = countdownToMain.ToString();
 		}
 	}
 }
